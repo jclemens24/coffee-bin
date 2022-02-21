b11a5d640155161bfa8c21e3fb838aeff90c2c40
Jordan Clemens <jclemens24@hotmail.com>
2022-02-21 10:05:57 -0500

    setup config and format

--- CAB_Coffee_Shop/public/js/index.js
+++ CAB_Coffee_Shop/public/js/index.js
@@ -11,65 +11,65 @@ const errorMessage = document.getElementById('message');
 let formIsValid = false;
 
 function validateForm() {
-	formIsValid = form.checkValidity();
-	if (formIsValid) {
-		errorMessage.textContent = 'Success!';
-		errorMessage.style.color = '#37b24d';
-	}
-	return formIsValid;
+  formIsValid = form.checkValidity();
+  if (formIsValid) {
+    errorMessage.textContent = 'Success!';
+    errorMessage.style.color = '#37b24d';
+  }
+  return formIsValid;
 }
 
-const checkValidity = function (el) {
-	if (
-		this.validity.patternMismatch ||
-		this.validity.typeMismatch ||
-		this.validity.valueMissing ||
-		this.validity.tooShort
-	) {
-		this.style.borderColor = '#f03e3e';
-		this.setCustomValidity(
-			this === emailInput
-				? 'Please enter a valid email address'
-				: `Please check the ${this.getAttribute(
-						'name'
-				  )} field. Format must match the example given`
-		);
-		this.reportValidity();
-	} else {
-		this.style.borderColor = '#37b24d';
-		this.setCustomValidity('');
-		return this.reportValidity();
-	}
+const checkValidity = function () {
+  if (
+    this.validity.patternMismatch ||
+    this.validity.typeMismatch ||
+    this.validity.valueMissing ||
+    this.validity.tooShort
+  ) {
+    this.style.borderColor = '#f03e3e';
+    this.setCustomValidity(
+      this === emailInput
+        ? 'Please enter a valid email address'
+        : `Please check the ${this.getAttribute(
+            'name'
+          )} field. Format must match the example given`
+    );
+    this.reportValidity();
+  } else {
+    this.style.borderColor = '#37b24d';
+    this.setCustomValidity('');
+    return this.reportValidity();
+  }
 };
 
 [
-	nameInput,
-	emailInput,
-	phoneInput,
-	birthdayInput,
-	streetAddress,
-	state,
-	zip,
+  nameInput,
+  emailInput,
+  phoneInput,
+  birthdayInput,
+  streetAddress,
+  state,
+  zip
 ].forEach(el => {
-	el.addEventListener('input', checkValidity.bind(el));
+  el.addEventListener('input', checkValidity.bind(el));
 });
 
 const processFormData = function (e) {
-	e.preventDefault();
-	validateForm();
-	if (formIsValid) {
-		const captureData = {
-			name: form.fullName.value,
-			email: form.email.value,
-			phone: form.phone?.value,
-			birthday: form.birthday.value,
-			streetAddress: form.streetAddress.value,
-			state: form.state.value,
-			zip: form.zip.value,
-		};
-		console.log(captureData);
-		window.location.assign(`/views/index.html`);
-	}
+  e.preventDefault();
+  validateForm();
+  if (formIsValid) {
+    const captureData = {
+      name: form.fullName.value,
+      email: form.email.value,
+      phone: form.phone.value,
+      birthday: form.birthday.value,
+      streetAddress: form.streetAddress.value,
+      state: form.state.value,
+      zip: form.zip.value
+    };
+    console.log(captureData);
+    window.location.assign(`/views/index.html`);
+  }
 };
 
 form.addEventListener('submit', processFormData);
