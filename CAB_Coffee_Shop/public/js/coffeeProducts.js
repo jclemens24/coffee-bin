5a0c222b5a4dcca200b29f95e6df37746fa2d5a0
Jordan Clemens <jclemens24@hotmail.com>
2022-03-01 12:07:26 -0500

    added cart implementation

--- CAB_Coffee_Shop/public/js/coffeeProducts.js
+++ CAB_Coffee_Shop/public/js/coffeeProducts.js
@@ -1,51 +1,143 @@
-import data from '../../data/product-details.json';
+import { data } from '../../data/product-details.js';
 
 console.log(data);
 
 const product = document.querySelectorAll('.dropdown-item');
 const modalBody = document.querySelector('.modal-body');
+class CartView {
+  _parentElement = document.querySelector('.modal-body');
+  _listElement = document.querySelector('.modal-items');
+  _cartTotals = document.querySelector('.cart-totals');
+  _message = '';
+  items = [];
+  totalItems = 0;
+  totalAmount = 0;
 
-let cart = {
-  totalItems: 0,
-  items: [],
-  totalAmount: 0
+  render() {
+    if (this.items.length === 0) {
+      return this.emptyCart();
+    }
+    const markup = this.items.map(this.generateMarkup);
+    this._listElement.insertAdjacentHTML('afterbegin', markup);
+    this._cartTotals.innerHTML = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      signDisplay: 'never'
+    }).format(this.calcTotalCartPrice());
+  }
+
+  generateMarkup(data) {
+    return `<li class="modal-item" id="${data.id}">
+              <img class="coffee_modal_img" src="/public/img/${
+                data.img
+              }" alt='${data.description}' />
+              <p>${data.category}</p>
+              <p>${data.description}</p>
+              <p>${new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD'
+              }).format(data.price)}</p>
+              <i class="bi bi-plus-circle inline-button" data-add="1" data-price=${
+                data.price
+              } data-id=${data.id}></i>
+              <span class="item-quantity">${data.quantity}</span>
+              <i class="bi bi-dash-circle inline-button" data-minus="1" data-price=${
+                data.price
+              } data-id=${data.id}></i>
+              </li>`;
+  }
+
+  update(data) {
+    this.calcNumberOfCartItems();
+    if (this.totalAmount === 0) {
+      this.emptyCart();
+    } else {
+      const oldDOM = document.getElementById(`${data.id}`);
+      oldDOM.querySelector('span').textContent = `${data.quantity}`;
+      this._cartTotals.innerHTML = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+      }).format(this.calcTotalCartPrice());
+    }
+  }
+
+  clear() {
+    this._listElement.innerHTML = '';
+    this._cartTotals.innerHTML = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(this.calcTotalCartPrice());
+  }
+
+  calcNumberOfCartItems() {
+    this.totalItems = this.items.reduce(
+      (prev, curr) => prev + curr.quantity,
+      0
+    );
+    return this.totalItems;
+  }
+
+  calcTotalCartPrice() {
+    this.totalAmount = this.items.reduce((acc, item) => {
+      const price = item.price;
+      const quantity = item.quantity;
+      return acc + price * quantity;
+    }, 0);
+
+    return this.totalAmount;
+  }
+
+  emptyCart() {
+    this.clear();
+    this._listElement.textContent = `You're cart is empty`;
+  }
+}
+
+const addItemToCart = function (id) {
+  const item = newCart.items.find(prod => prod.id === +id);
+  item.quantity++;
+  newCart.update(item);
+};
+
+const removeItemFromCart = function (id) {
+  const item = newCart.items.find(prod => prod.id === +id);
+  if (item.quantity === 1) {
+    newCart.items = newCart.items.filter(prod => prod.id !== +id);
+    newCart.clear();
+    newCart.render();
+  }
+  if (item.quantity > 1) {
+    console.log(newCart.items);
+    item.quantity--;
+    newCart.update(item);
+  }
 };
 
 modalBody.addEventListener('click', function (e) {
   const button = e.target.closest('.inline-button');
   if (!button) return;
-  console.log(button);
-  if (button.dataset.add) {
-    cart.totalItems++;
-    console.log(cart);
-  } else {
-    cart.totalItems--;
-    console.log(cart);
-  }
+  button.dataset.add
+    ? addItemToCart(button.dataset.id)
+    : removeItemFromCart(button.dataset.id);
 });
 
-function addToCart(item) {
-  const text = `<div class="modal-item">
-                  <img class="coffee_modal_img" src="/${item.img}" alt='${item.description}' />
-                  <p>${item.category}</p>
-                  <p>${item.description}</p>
-                  <p>${item.price}</p>
-                  <i class="bi bi-plus-circle inline-button" data-add="1"></i>
-                  <i class="bi bi-dash-circle inline-button" data-minus="1"></i>
-              </div>
-  `;
-  return modalBody.insertAdjacentHTML('beforeend', text);
-}
+const initializeCart = function (e) {
+  const id = e.target.dataset.id;
+  const itemAlreadyInCart = newCart.items.find(item => item.id === +id);
+  if (itemAlreadyInCart) {
+    itemAlreadyInCart.quantity++;
+    newCart.update(itemAlreadyInCart);
+  } else {
+    const getItem = data.find(item => item.id === +id);
+    getItem.quantity = 1;
+    newCart.items.push(getItem);
+    newCart.clear();
+    newCart.render();
+  }
+};
+
+const newCart = new CartView();
 
 product.forEach(el => {
-  el.addEventListener('click', function (e) {
-    e.preventDefault();
-    const id = e.target.dataset.id;
-    const foundItem = data.find(prod => prod.id === +id);
-    cart.items.push(foundItem);
-    cart.totalItems++;
-    cart.totalAmount = foundItem.price;
-    console.log(cart);
-    addToCart(foundItem);
-  });
+  el.addEventListener('click', initializeCart);
 });
